8e1e08a765457cd872d906983df134a1ceaaf7ab
chinhbean09 <chinh0726@gmail.com>
2023-10-28 23:01:14 +0700

    NavBar

---
+++ 172667-2/src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { toast } from "react-toastify";
+
+const Navbar = () => {
+    const [isLogin, setIsLogin] = useState(false);
+    function SignOut() {
+        setIsLogin(false);
+        localStorage.removeItem('isLogin');
+        toast.success("Logout succesfully");
+    }
+    function SignIn() {
+        setIsLogin(true);
+        localStorage.setItem('isLogin', 'islogin');
+        toast.success("Login succesfully");
+    }
+    return (
+        <AppBar position='static'>
+            <Toolbar>
+                <Button color='inherit' component={Link} to="/">Home</Button>
+                <Button color='inherit' component={Link} to="/dashboard">Dashboard</Button>
+                <Button color='inherit' component={Link} to="/contact">Contact</Button>
+                    {!isLogin ? (
+                        <Button color='inherit' component={Link} to="/"
+                            onClick={SignIn}> Login
+                        </Button>
+                    ) : (<Button color='inherit' component={Link} to="/"
+                        onClick={SignOut}> Logout
+                    </Button>)
+                    }
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navbar;
